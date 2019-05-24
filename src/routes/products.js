6dd83f070960f7932753a58de9e6cd01fa51b836
shoudaos <shoudaos92@gmail.com>
2019-05-24 18:06:04 +0300

    feat(Products): Add CRUD

--- src/routes/products.js
+++ src/routes/products.js
@@ -10,4 +10,36 @@ router.get('/', (req, res) => {
 
 });
 
+router.get('/:id', (req, res) => {
+    Product.findByPk(req.params.id).then(product => {
+        res.json(product);
+    })
+
+});
+
+router.put('/:id', (req, res) => {
+    Product.update(req.body, {where: {id: req.params.id}}).then(([product]) => {
+        res.json(product);
+    })
+
+});
+
+router.post('/', (req, res) => {
+    let product = req.body;
+    if (product.id) {
+        delete product.id;
+    }
+    Product.create(req.body).then(product => {
+        res.json(product);
+    })
+
+});
+
+router.delete('/:id', (req, res) => {
+    Product.destroy({where: {id: req.params.id}}).then(response => {
+        res.json(response);
+    })
+
+});
+
 module.exports = router;
