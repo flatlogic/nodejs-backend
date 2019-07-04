e502cf733bca13e444f5357eae3be2466e71ab4f
shoudaos <shoudaos92@gmail.com>
2019-07-04 11:37:43 +0300

    feat(products): added schedule for updating products list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import passport from "passport";
+import cron from "node-cron";
+
+const {exec} = require('child_process');
 
 import products from"./routes/products";
 import analytics from"./routes/analytics";
@@ -25,7 +28,14 @@ app.use('/products', passport.authenticate("jwt", {session: false}), products);
 app.use('/analytics', passport.authenticate("jwt", {session: false}), analytics);
 app.use('/user', user);
 db.sequelize.sync().then(() => {
-    app.listen(port, function () {
-        console.log(`Example app listening on port ${port}!!`);
+  app.listen(port, function () {
+      console.log(`Example app listening on port ${port}!!`);
+  });
+  cron.schedule('* */2 * * *', () => {
+    exec('yarn products:update', (err) => {
+      if (err) {
+        console.error(err);
+      }
     });
+  });
 });
